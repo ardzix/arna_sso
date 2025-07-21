a3e104d7328f80dd06620c114d3ee95af437a5b2
Arif Dzikrullah <ardzyix@gmail.com>
2025-07-21 21:18:48 +0700

    fix: remove auditlog signal

--- authentication/signals.py
+++ authentication/signals.py
@@ -1,7 +1,6 @@
 from django.dispatch import receiver, Signal
 from django_q.tasks import async_task
 from authentication.models import User
-from audit_logging.models import AuditLog
 from datetime import timedelta
 from django.utils.timezone import now
 from .libs.utils import send_otp_email as soe, generate_otp
@@ -18,38 +17,34 @@ user_token_refreshed = Signal()
 # Signal handlers
 @receiver(user_registered)
 def handle_user_registration(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'register', metadata)
     async_task(send_otp_email, metadata.get('email'))
 
 @receiver(user_logged_in)
 def handle_user_login(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'login', metadata)
+    pass
 
 @receiver(user_logged_out)
 def handle_user_logout(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'logout', metadata)
+    pass
 
 @receiver(mfa_setup)
 def handle_mfa_setup(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'set_mfa', metadata)
+    pass
 
 # Receiver for mfa_verified
 @receiver(mfa_verified)
 def handle_mfa_verified(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'mfa_verified', metadata)
+    pass
 
 # Receiver for mfa_login_attempt
 @receiver(mfa_login_attempt)
 def handle_mfa_login_attempt(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'mfa_login_attempt', metadata)
+    pass
 
 # Receiver for user_token_refreshed
 @receiver(user_token_refreshed)
 def handle_token_refresh(sender, user, metadata, **kwargs):
-    async_task(log_audit, user, 'token_refresh', metadata)
-
-def log_audit(user, action, metadata):
-    AuditLog.objects.create(user=user, action=action, metadata=metadata)
+    pass
 
 def send_otp_email(email):
     otp = generate_otp()
